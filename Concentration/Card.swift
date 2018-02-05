0f2ecadf62583b077f509d21a7461df81c5d447f
celiagomezdev <celiagomezdev@gmail.com>
2018-02-05 17:33:40 +0100

    Prepare MVC architecture

---
+++ Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Celia Gómez de Villavedón on 05/02/2018.
+//  Copyright © 2018 Celia Gómez de Villavedón Pedrosa. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init(identifier: Int) {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var game = Concentration()
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips = \(flipCount)"
+        }
     }
+    
+    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("this button is not properly connected")
+        }
     }
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 
 }
 
