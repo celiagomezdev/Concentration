f1323bf4fd41c8f2885a7e30478ad9982cb393f8
celiagomezdev <celiagomezdev@gmail.com>
2018-03-16 17:25:50 +0100

    Discard random theme choose

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class ConcentrationThemeChooserViewController: UIViewController {
 
-
+    //Themes
+    private var faces : [String:Any] = ["emojis" : "😱😳😭😜😘😅😍😎😂", "backgroundColor": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), "cardColor": #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), "playAgainButton": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) ]
+    
+    private var halloween : [String:Any] = ["emojis" : "👻🎃🙀🦇🍭🍬🍎😈😱", "backgroundColor": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), "cardColor": #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), "playAgainButton": #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) ]
+    
+    private var nature : [String:Any] = ["emojis" : "🐸🐛🌸🍄🌻🕷🐌🌳🌹", "backgroundColor": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), "cardColor": #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), "playAgainButton": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) ]
+    
+    private var party : [String:Any] = ["emojis" : "🎊🎈🎉🍻🍾🍸🕺💃🏻🤪", "backgroundColor": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), "cardColor": #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), "playAgainButton": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) ]
+    
 
     /*
     // MARK: - Navigation
--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -21,13 +21,13 @@ class ConcentrationViewController: UIViewController {
     @IBOutlet private weak var playAgainButton: UIButton!
     
     private var faces : [String:Any] = ["emojis" : "😱😳😭😜😘😅😍😎😂", "backgroundColor": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), "cardColor": #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), "playAgainButton": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) ]
-    
+
     private var halloween : [String:Any] = ["emojis" : "👻🎃🙀🦇🍭🍬🍎😈😱", "backgroundColor": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), "cardColor": #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), "playAgainButton": #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) ]
-    
+
     private var nature : [String:Any] = ["emojis" : "🐸🐛🌸🍄🌻🕷🐌🌳🌹", "backgroundColor": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), "cardColor": #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), "playAgainButton": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) ]
-    
+
     private var party : [String:Any] = ["emojis" : "🎊🎈🎉🍻🍾🍸🕺💃🏻🤪", "backgroundColor": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), "cardColor": #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), "playAgainButton": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) ]
-    
+
     lazy var themes = [halloween, nature, party, faces]
     
     private var selectedTheme: [String:Any]!
@@ -78,31 +78,34 @@ class ConcentrationViewController: UIViewController {
         return emoji[card] ?? "?"
     }
     
-    private func selectNewTheme() {
-        let randomIndex = Int(arc4random_uniform(UInt32(themes.count)))
-        selectedTheme = themes[randomIndex]
-        
-        if let backgroundColor = selectedTheme["backgroundColor"] as? UIColor {
-            view.backgroundColor = backgroundColor
-        }
-        
-        if let playAgainButtonColor = selectedTheme["playAgainButton"] as? UIColor {
-            playAgainButton.setTitleColor(playAgainButtonColor, for: UIControlState.normal)
-        }
-        
-        if let labelTextColor = selectedTheme["cardColor"] as? UIColor {
-            flipCountLabel.textColor = labelTextColor
-        }
-        
+    private func renewEmojis() {
+        emojiChoices.removeAll()
+        emoji.removeAll()
         emojiChoices = selectedTheme["emojis"] as! String
     }
     
+//    private func selectNewTheme() {
+//        let randomIndex = Int(arc4random_uniform(UInt32(themes.count)))
+//        selectedTheme = themes[randomIndex]
+//
+//        if let backgroundColor = selectedTheme["backgroundColor"] as? UIColor {
+//            view.backgroundColor = backgroundColor
+//        }
+//
+//        if let playAgainButtonColor = selectedTheme["playAgainButton"] as? UIColor {
+//            playAgainButton.setTitleColor(playAgainButtonColor, for: UIControlState.normal)
+//        }
+//
+//        if let labelTextColor = selectedTheme["cardColor"] as? UIColor {
+//            flipCountLabel.textColor = labelTextColor
+//        }
+//
+//        emojiChoices = selectedTheme["emojis"] as! String
+//    }
+    
         
     @IBAction private func playAgain(_ sender: UIButton) {
-        emojiChoices.removeAll()
-        emoji.removeAll()
-        selectNewTheme()
-   
+        renewEmojis()
         game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
         updateViewFromModel()
     }
